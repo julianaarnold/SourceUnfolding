abada759b1d0672cf3d2a17795263e0e7b222427
Julian Arnold <julian.arnold@student.hpi.de>
2023-12-18 22:23:04 +0100

    rename stuff

--- star_unfolding.py
+++ star_unfolding.py
@@ -1,5 +1,5 @@
 from basic_unfolding import BasicUnfolding
 
-class VertexUnfolding(BasicUnfolding):
+class StarUnfolding(BasicUnfolding):
     def __init__(self, verts, faces):
         super().__init__(verts, faces)
