f35fb7ccd3a60ca8d26da129a013f21441dd9766
Julian Arnold <julian.arnold@student.hpi.de>
2024-01-07 17:34:44 +0100

    add pyramid test mesh

--- run.py
+++ run.py
@@ -8,10 +8,10 @@ from basic_unfolding import BasicUnfolding
 from star_unfolding import StarUnfolding
 from source_unfolding import SourceUnfolding
 
-raw_vertices, raw_faces = igl.read_triangle_mesh("./meshes/cube.stl")
+raw_vertices, raw_faces = igl.read_triangle_mesh("./meshes/pyramid.stl")
 vertices, faces = igl.remove_duplicates(raw_vertices, raw_faces, 0.00001)
 
-unfolding = SourceUnfolding(vertices, faces, [.8, .8, 1], show_intermediate_results=True, report_errors=True)
+unfolding = SourceUnfolding(vertices, faces, [0, 0, -1], show_intermediate_results=True, report_errors=True)
 
 plot_polygons(unfolding.unfolded_polygons.values())
 plot_cut_edges(unfolding.unfolded_polygons, unfolding.faces, unfolding.faces_to_separate)
